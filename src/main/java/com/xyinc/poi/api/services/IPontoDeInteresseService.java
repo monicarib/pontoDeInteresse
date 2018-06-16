8dadff4e9e7d7bc9bfa39379b1cc7a4a7850ef92
Mônica Ribeiro <monicarib1@gmail.com>
2018-06-16 18:00:55 -0300

    Refatorando a assinatura do método listarPorProximidade da interface IPontoDeInteresseService.

--- src/main/java/com/xyinc/poi/api/services/IPontoDeInteresseService.java
+++ src/main/java/com/xyinc/poi/api/services/IPontoDeInteresseService.java
@@ -3,9 +3,10 @@ package com.xyinc.poi.api.services;
 import java.util.List;
 
 import com.xyinc.poi.api.documents.PontoDeInteresse;
+import com.xyinc.poi.api.requests.ListarPorProximidadeRequest;
 
 public interface IPontoDeInteresseService {
 	PontoDeInteresse cadastrar(PontoDeInteresse pontoDeInteresse);
 	List<PontoDeInteresse> listarTodos();
-	List<PontoDeInteresse> listarPorProximidade(String pontoX, String pontoY);
+	List<PontoDeInteresse> listarPorProximidade(ListarPorProximidadeRequest listarPorProximidadeRequest);
 }
