c450eace4212b710d4e93129466298aee00ea975
Mônica Ribeiro <monicarib1@gmail.com>
2018-06-16 18:00:11 -0300

    Adicionando função de listar de acordo com a localização no Repository.

--- src/main/java/com/xyinc/poi/api/repositories/PontoDeInteresseRepository.java
+++ src/main/java/com/xyinc/poi/api/repositories/PontoDeInteresseRepository.java
@@ -1,9 +1,12 @@
 package com.xyinc.poi.api.repositories;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.geo.Distance;
+import com.mongodb.client.model.geojson.Point;
 import com.xyinc.poi.api.documents.PontoDeInteresse;
 
 public interface PontoDeInteresseRepository extends MongoRepository<PontoDeInteresse, String> {
-	
+	public List<PontoDeInteresse> findByLocalizacaoNear(org.springframework.data.geo.Point ponto, Distance distance);
 }
