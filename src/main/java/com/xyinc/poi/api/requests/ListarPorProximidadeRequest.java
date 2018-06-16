8ff969820b7792d0073f7eee730f344901e44284
Mônica Ribeiro <monicarib1@gmail.com>
2018-06-16 17:59:28 -0300

    Adicionando entidade para representar o request da requisição listarPorProximidade.

---
+++ src/main/java/com/xyinc/poi/api/requests/ListarPorProximidadeRequest.java
@@ -0,0 +1,43 @@
+package com.xyinc.poi.api.requests;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import com.xyinc.poi.api.documents.Localizacao;
+
+public class ListarPorProximidadeRequest {
+	private Integer distanciaMaxima;
+	private Integer pontoX;
+	private Integer pontoY;
+	
+	@NotNull(message = "O atributo pontoX não pode ser vazio.")
+	@Min(value = 0, message = "O atributo pontoX deve ser um inteiro positivo.")
+	public Integer getPontoX() {
+		return pontoX;
+	}
+
+	public void setPontoX(Integer pontoX) {
+		this.pontoX = pontoX;
+	}
+	
+	@NotNull(message = "O atributo pontoY não pode ser vazio.")
+	@Min(value = 0, message = "O atributo pontoY deve ser um inteiro positivo.")
+	public Integer getPontoY() {
+		return pontoY;
+	}
+
+	public void setPontoY(Integer pontoY) {
+		this.pontoY = pontoY;
+	}
+	
+	@NotNull(message = "O atributo distância máxima não pode ser vazio.")
+	@Min(value = 0, message = "O atributo distância máxima deve ser um inteiro positivo.")
+	public Integer getDistanciaMaxima() {
+		return distanciaMaxima;
+	}
+	
+	public void setDistanciaMaxima(Integer distanciaMaxima) {
+		this.distanciaMaxima = distanciaMaxima;
+	}
+}
